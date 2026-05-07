296d1913e361fb3b1e2c062cd42abdb1a9c55b3c
Jonas Vacek <jvacek@users.noreply.github.com>
2026-05-07 13:25:55 +0200

    adjust old migration for unnecessary import

--- flamerelay/users/migrations/0001_initial.py
+++ flamerelay/users/migrations/0001_initial.py
@@ -1,10 +1,7 @@
 import django.contrib.auth.models
 import django.contrib.auth.validators
 import django.utils.timezone
-from django.db import migrations
-from django.db import models
-
-import flamerelay.users.models
+from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
